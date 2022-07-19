e5ff9dfcda166ca1502f80a24f05deb2622f0bff
최호준 <hojoon90@gmail.com>
2022-07-19 23:53:25 +0900

    Command 패턴 작성 완료

---
+++ 디자인패턴/22. Command/src/example/command/Command.java
@@ -0,0 +1,5 @@
+package example.command;
+
+public interface Command {
+    public abstract void execute();
+}
---
+++ 디자인패턴/22. Command/src/example/command/Drawable.java
@@ -0,0 +1,5 @@
+package example.command;
+
+public interface Drawable {
+    public abstract void draw(int x, int y);
+}
